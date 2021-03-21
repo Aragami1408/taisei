c3469934784b46447440d8b17d69cc9d3fabec8b
Alice D <alice@starwitch.productions>
2021-03-21 18:11:29 -0400

    finish porting over last basic enemy, begin more work on porting midboss

---
+++ src/stages/stage5/entities.h
@@ -0,0 +1,17 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+*/
+
+#ifndef IGUARD_stages_stage5_entities_h
+#define IGUARD_stages_stage5_entities_h
+
+#include "taisei.h"
+
+#define ENTITIES_STAGE5(X, ...) \
+	X(IkuSlave, __VA_ARGS__) \
+
+#endif // IGUARD_stages_stage5_entities_h
